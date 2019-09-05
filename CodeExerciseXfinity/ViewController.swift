c7bb15a3e45de700bada06bfe3bec3c751db2b86
DanielAlert <alertwisdomsolutions@gmail.com>
2019-09-05 10:42:50 -0400

    Initial Commit

---
+++ CodeExerciseXfinity/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CodeExerciseXfinity
+//
+//  Created by DANIEL SPADY on 9/5/19.
+//  Copyright © 2019 DANIEL SPADY. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
